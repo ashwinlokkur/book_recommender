5a974db392aa283b0b4604307773c559f90b7ce5
Ashwin Lokkur <ash.lokkur@gmail.com>
2017-11-13 18:49:14 +0530

    Done by me(Ashwin) Added CSV reader of User data Creating a dictionary in the format: {user_id:{genre:liking}}

--- reader.py
+++ reader.py
@@ -1,14 +1,42 @@
 import csv 
 
 data = {}
+user={}
+genres=set([])
 
-def fill() :
+def get() :
+	global data
+	global genres
 	with open("bookdbfinal2.csv") as db:
 		reader = csv.DictReader(db)
 		for row in reader:
 			key = row['book_id']
-			value = row['Avg_Rating']
+			rowData = {"author_name":row['author_name'],"author_movement":row['author_movement'],"genre":row['genre'],"abstract":row['abstract'],"Avg_rating":row['Avg_Rating']}
+			genres.add(row['genre'])
+			value = rowData
 			data[key] = value
 
-fill()
-# print(data)	
+def getUser() :
+	global user
+	with open("userCSV.csv") as db:
+		reader = csv.DictReader(db)
+		for row in reader:
+			key = row['user_id']
+			if not user.has_key(key):
+				user[key]={}
+			rowData = {row['genre']:row['like']}
+			value = rowData
+			#print user[key]
+			user[key].update(value)
+	for i in range(1, len(user)+1):
+		key = str(i)
+		for g in genres:
+			if not user[str(i)].has_key(g):
+				value = {g:0}
+				user[key].update(value)
+				
+
+get()
+getUser()
+#print data['1']['genre']
+print len(user[str(1)])
