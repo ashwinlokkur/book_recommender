c99d885168422f9ccaf3bc6de8176c3f1c98145d
Aniket Panda <Aniket@Anikets-MacBook-Pro.local>
2017-11-14 14:49:22 +0530

    Calculated the most similar user Used Euclidean Distance to calualte similarity matrix

---
+++ similarity.py
@@ -0,0 +1,46 @@
+# this generates a similarity matrix
+
+import readers
+import numpy as np
+import math
+
+n = len(readers.userLiking)
+
+userLiking1 = {}
+simMatrix = np.zeros((n,n))
+
+for i in sorted(readers.userLiking.keys()):
+	userLiking1[i] = ''.join(str(e) for e in readers.userLiking[i])
+
+def dif(i,j):
+	m = len(userLiking1[i])
+	diff = 0
+	for k in range(m):
+		diff += (int(userLiking1[i][k]) - int(userLiking1[j][k]))**2
+	return math.sqrt(diff)
+
+def similarityMatrix():
+	for i in sorted(readers.userLiking.keys()):
+		for j in sorted(readers.userLiking.keys()):
+			simMatrix[int(i)-1,int(j)-1] = (dif(i,j)) 	
+	print simMatrix
+
+similarityMatrix()
+
+ip = raw_input("What user do you want to find similarity to? please eneter between 1 and 3 \n")
+ 
+if ip not in readers.userLiking.keys():
+	print("invalid input")
+
+
+k = (sorted(readers.userLiking.keys()))
+k.remove(ip)
+
+y = "" 
+x = 9.7
+for i in k:
+	if(x > simMatrix[int(ip)-1,int(i)-1]):
+		x = simMatrix[int(ip)-1,int(i)-1]
+		y = i
+
+print("user "+ip+" is similar to user "+y)		
