a765687f5600a3132866e25628a2643ab11729f0
Aniket Panda <Aniket@Anikets-MacBook-Pro.local>
2017-11-19 21:20:57 +0530

    added test.py which implements python library flask that renders index.php present in the template folder after executing test.py open browser and go to url 127.0.0.0:5000

---
+++ test.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import render_template,request
+import csv
+# import similarity
+
+app = Flask(__name__)
+
+top_10 = ['Adventure','Biography','Comedy','Drama','Fiction','History','Literature','Mythology','Suspense','Thriller']
+genres = []
+sel = [1,2,3,4,5]
+selected_genres =[]
+
+with open("bookdbfinal2.csv") as db:
+		reader = csv.DictReader(db)
+		for row in reader:
+			m = row['genre'].decode("utf-8")
+			if m not in genres:
+				genres.append(m)		
+
+@app.route('/', methods =['GET','POST'])
+def generate():
+    return render_template('index.html',genres=genres, top_10 =top_10,sel = sel)
+
+
+@app.route("/forward/", methods=['GET','POST'])
+def foo():
+	print 'hi'
+	for i in range(5):
+		n = "top"+str(i+1)
+		selected_genres.append(request.form[n])
+		print selected_genres
+	for i in range(2):
+		n = "genre"+str(i+1)
+		selected_genres.append(request.form[n])	
+		print selected_genres
+def rend():
+	return render_template('index.html',genres=genres, top_10 =top_10,sel = sel)
+	# print selected_genres
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
+
+
